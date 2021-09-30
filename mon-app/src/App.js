2cd86bc52cbfdfecd39a93cb70b13150f09044d9
samSOUMAH <sam86soumah@gmail.com>
2021-09-30 15:27:42 +0200

    composanst react

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Button from 'react-bootstrap/Button';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ListVille } from './ListVille';
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Bonjour</h1>
-      <Button variant="danger"></Button>
-      <h3><FontAwesomeIcon icon={faSpinner} color="blue" spin/></h3>
+      <h1 className="d-flex justify-content-center">VILLES DU MAROC</h1>
+      <ListVille></ListVille>
   
     </div>
   );
---
+++ mon-app/src/ListVille.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ItemsVilles } from './ItemsVilles'
+
+export const ListVille = () => {
+    return (
+        <div >
+       <ul className="d-flex justify-content-around">
+           <ItemsVilles />
+
+       </ul>
+        </div>
+    )
+}
