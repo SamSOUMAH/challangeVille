c89d2b58d46eae768cc7e98e2d4a49d95ba3681d
samSOUMAH <sam86soumah@gmail.com>
2021-10-27 00:02:23 +0200

    Redux 1ere Lecon

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,29 +1,10 @@
 import React from 'react'
-import { useState, useRef } from 'react'
-import ListeTitre from './ListeTitre'
-import AjouElem from './AjouElem'
-
-
 
 const App = () => {
-  const [Lestitres, setLestitres] = useState([{id:1, titre:"Bonjour"}, {id:2, titre:"Salut"}, {id:3, titre:"Nuit"}])
-  const suppElement=(idTite)=>{
-    if (window.confirm("VOULEZ VOUS SUPPRIMER?")===false) return 
-
-    let nouvellle=[...Lestitres]
-    nouvellle=nouvellle.filter((t)=>t.id!=idTite)
-    setLestitres([...nouvellle])
-  } 
-
-
+ 
   return (
     <div>
-      <AjouElem />
-      
- <ListeTitre
-  Lestitres={Lestitres}
-  suppElem={suppElement}
- />
+    Bonjour le Monde
     </div>
   )
 }
--- mon-app/src/index.js
+++ mon-app/src/index.js
@@ -3,16 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+import { Task } from "./models/task";
+
+const initialState = [
+  new Task(1, "task 1"),
+  new Task(2, "task 2"),
+  new Task(3, "task 3")
+]
+
+const TodoReducer = (prevState, typeAction, inputAction) => {
+  // if (typeAction == "todo/add") {
+  //   return [
+  //     ...state.todos,
+  //     new Task(
+  //       state.todos.length + 1,
+  //       inputAction.titleTask
+  //     )
+  //   ]
+
+  // }
+  // else if (typeAction == "todo/delete") {
+
+  // }
+  // else if (typeAction == "todo/toggle") {
+
+  // } else {
+  //   return state;
+  // }
+
+  switch (typeAction) {
+    case "todo/add":
+      return [...prevState, new Task(
+        prevState.length + 1,
+          inputAction.titleTask
+        )
+      ]
+    
+    default:
+      return prevState
+  }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+let newState=TodoReducer(initialState,"todo/add", {titleTask:"task 4"})
+console.log(newState)
+
+let newState1=TodoReducer(newState,"todo/add", {titleTask:"task 5"})
+console.log(newState1)
