6df73eba3eaa967465f16c7a4e535ad00e035096
samSOUMAH <sam86soumah@gmail.com>
2021-10-27 16:15:02 +0200

    fix order of parametres done redux

--- mon-app/src/index.js
+++ mon-app/src/index.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App';
-
-
 import { Task } from "./models/task";
 
 const initialState = [
@@ -13,46 +6,40 @@ const initialState = [
   new Task(3, "task 3")
 ]
 
-const TodoReducer = (prevState, typeAction, inputAction) => {
-  // if (typeAction == "todo/add") {
-  //   return [
-  //     ...state.todos,
-  //     new Task(
-  //       state.todos.length + 1,
-  //       inputAction.titleTask
-  //     )
-  //   ]
+const TYPE_TODO_ADD="todo/add"
 
-  // }
-  // else if (typeAction == "todo/delete") {
+const TodoReducer = (action={},prevState=initialState ) => {
 
-  // }
-  // else if (typeAction == "todo/toggle") {
-
-  // } else {
-  //   return state;
-  // }
-
-  switch (typeAction) {
-    case "todo/add":
-      return [...prevState, new Task(
-        prevState.length + 1,
-          inputAction.titleTask
+  switch (action.type) {
+   
+    case TYPE_TODO_ADD:
+      return [
+        ...prevState,
+        new Task(
+          prevState.length + 1,
+          action.payload.titleTask
         )
       ]
-    
     default:
       return prevState
   }
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-
-let newState=TodoReducer(initialState,"todo/add", {titleTask:"task 4"})
-console.log(newState)
-
-let newState1=TodoReducer(newState,"todo/add", {titleTask:"task 5"})
+let newState1 = 
+TodoReducer(
+  {
+    type : TYPE_TODO_ADD,
+    payload : {titleTask:"task 4"}
+  }
+)
 console.log(newState1)
+
+let newState2 = 
+TodoReducer(
+  {
+    type : TYPE_TODO_ADD,
+    payload : {titleTask:"task 5"}
+  },
+  newState1
+)
+console.log(newState2)
